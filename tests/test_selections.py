725871cb1d8205f2fd372293ba6c6a2f66458a0f
Zia Rauwolf <toolgirl-github@tentacle.net>
2018-05-05 13:42:04 -0700

    WIP Test Selections

--- tests/test_selections.py
+++ tests/test_selections.py
@@ -6,10 +6,28 @@ from tunescope.selections import SelectionList, Selection
 class TestSelection(object):
     def test_constructor(self):
         s = Selection()
-        assert s.number == 1
         assert s.name == ''
         assert s.start == 0.0
         assert s.end == 0.0
+
+
+def make_three_selection_list():
+    sl = SelectionList()
+    sl.current.name = 'first'
+    sl.current.start = 1.0
+    sl.current.end = 2.0
+
+    sl.add()
+    sl.current.name = 'second'
+    sl.current.start = 3.0
+    sl.current.end = 4.0
+
+    sl.add()
+    sl.current.name = 'third'
+    sl.current.start = 5.0
+    sl.current.end = 6.0
+
+    return sl
 
 
 class TestSelectionList(object):
@@ -26,15 +44,79 @@ class TestSelectionList(object):
         # should append new with incremented number (be sure to account for
         # deleted selections), blank name, same start and end as current; new
         # should become current
-        pass
+        sl = SelectionList()
+        sl.current.name = 'first'
+        sl.current.start = 1.0
+        sl.current.end = 2.0
 
-    def test_delete(self):
-        # should refuse to delete last selection
-        pass
+        sl.add()
 
-    def test_set_current(self):
+        assert len(sl.selections) == 2
+        assert sl.selections[1] == sl.current
+        assert sl.current.number == 2
+        assert sl.current.name == ''
+        assert sl.current.start == 1.0
+        assert sl.current.end == 2.0
+
+    def test_delete_last_remaining_selection(self):
+        sl = SelectionList()
+        sl.current.number = 2
+        sl.current.name = 'second'
+        sl.current.start = 1.0
+        sl.current.end = 2.0
+
+        sl.delete(sl.current)
+
+        assert len(sl.selections) == 1
+        assert sl.selections[0] == sl.current
+        assert sl.current.number == 1
+        assert sl.current.name == ''
+        assert sl.current.start == 0.0
+        assert sl.current.end == 0.0
+
+    def test_delete_nonexistent_selection(self):
+        sl = SelectionList()
+        sl.current.number = 2
+
+        s = Selection(number=99)
+        sl.delete(s)
+
+        assert len(sl.selections) == 1
+        assert sl.current.number == 2
+
+    def test_delete_not_current(self):
+        sl = make_three_selection_list()
+        sl.current = sl.selections[0]
+        selection_to_delete = sl.selections[2]
+        sl.delete(selection_to_delete)
+
+        assert selection_to_delete not in sl.selections
+        assert len(sl.selections) == 2
+
+    def test_delete_current_when_current_not_last(self):
+        sl = make_three_selection_list()
+        sl.current = sl.selections[1]
+        sl.delete(sl.current)
+
+        assert len(sl.selections) == 2
+        assert sl.current == sl.selections[1]
+
+    def test_delete_current_when_current_is_last(self):
+        sl = make_three_selection_list()
+        sl.current = sl.selections[-1]
+        sl.delete(sl.current)
+
+        assert len(sl.selections) == 2
+        assert sl.current == sl.selections[1]
+
+    def test_set_current_to_nonexistent_selection(self):
         # should reject if the value being set is not a selection in the list
-        pass
+        sl = SelectionList()
+        s = Selection()
+        # with pytest.raises(ValueError):
+        #     sl.current = s
+
+
 
     def test_sort(self):
         pass
--- tunescope/selections.py
+++ tunescope/selections.py
@@ -3,7 +3,7 @@ from kivy.properties import NumericProperty, ListProperty, ObjectProperty, Strin
 
 
 class Selection(EventDispatcher):
-    number = NumericProperty(1)
+    number = NumericProperty()
     name = StringProperty('')
     start = NumericProperty(0.0)
     end = NumericProperty(0.0)
@@ -20,4 +20,36 @@ class SelectionList(EventDispatcher):
         super(SelectionList, self).__init__(**kwargs)
         self.current = Selection(number=1)
         self.selections.append(self.current)
-        self.next_selection_number = 2
+
+    def add(self):
+        selection = Selection(
+            number=max([s.number for s in self.selections]) + 1,
+            start=self.current.start,
+            end=self.current.end)
+        self.selections.append(selection)
+        self.current = selection
+
+    def delete(self, selection):
+        if selection not in self.selections:
+            return
+
+        if len(self.selections) == 1:
+            self.current.number = 1
+            self.current.name = ''
+            self.current.start = 0.0
+            self.current.end = 0.0
+            return
+
+        if selection == self.current:
+            current_index = self.selections.index(self.current)
+            if current_index == len(self.selections) - 1:
+                new_current_index = current_index - 1
+            else:
+                new_current_index = current_index + 1
+            self.current = self.selections[new_current_index]
+
+        self.selections.remove(selection)
+
+    def on_current(self, instance, value):
+        if value not in self.selections:
+            raise ValueError()
