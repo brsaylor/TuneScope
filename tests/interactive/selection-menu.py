c6bf9efd355ab80c38c90e22fdb87e5c4fe415ad
Zia Rauwolf <toolgirl-github@tentacle.net>
2018-05-12 21:22:09 -0700

    WIP Selection Menu

---
+++ tests/interactive/selection-menu.py
@@ -0,0 +1,30 @@
+from kivy.uix.boxlayout import BoxLayout
+from kivy.base import runTouchApp
+from kivy.uix.label import Label
+
+from tunescope.widgets.selectionmenu import SelectionMenu
+from tunescope.selections import SelectionList
+
+class RootWidget(BoxLayout):
+
+    def __init__(self, **kwargs):
+        super(RootWidget, self).__init__(orientation='vertical')
+        sl = SelectionList()
+        sl.current.name = 'first'
+        sl.current.start = 1.0
+        sl.current.end = 2.0
+
+        sl.add()
+        sl.current.name = 'second'
+        sl.current.start = 3.0
+        sl.current.end = 4.0
+
+        sl.add()
+        sl.current.name = 'third'
+        sl.current.start = 5.0
+        sl.current.end = 6.0
+        self.add_widget(SelectionMenu(selection_list=sl))
+
+
+root_widget = RootWidget()
+runTouchApp(root_widget)
--- tunescope/widgets/selectionmenu.py
+++ tunescope/widgets/selectionmenu.py
@@ -1,9 +1,38 @@
-from kivy.uix.modalview import ModalView
+import os.path
+
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.label import Label
+from kivy.uix.modalview import ModalView
+from kivy.uix.widget import Widget
+from kivy.lang.builder import Builder
+from kivy.properties import ObjectProperty
+
+from ..selections import SelectionList
 
 
-class SelectionMenu(ModalView):
-    def __init__(self, selections, selection_index, on_confirm, **kwargs):
+class SelectionMenu(BoxLayout):
+    selection_list = ObjectProperty(SelectionList(), rebind=True)
+    def __init__(self, **kwargs):
         super(SelectionMenu, self).__init__(**kwargs)
+        add = self.ids.grid.add_widget
+        for s in self.selection_list.selections:
+            add(Cell(text=str(s.number)))
+            add(Cell(text=s.name))
+            add(Cell(text=str(s.start)))
+            add(Cell(text=str(s.end)))
+            add(Cell(text='Trash'))
 
-        
+    def on_header_press(self, *args):
+        print(args)
+
+
+class Header(ButtonBehavior, BoxLayout):
+    pass
+
+class Cell(BoxLayout):
+    pass
+
+Builder.load_file(os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'selectionmenu.kv'))
