ad4d76e39413a308c41509f1875623ebd08f5b9a
Zia Rauwolf <toolgirl-github@tentacle.net>
2018-05-19 16:46:39 -0700

    Fix deleting selections from selection menu

--- tunescope/widgets/selectionmenu.py
+++ tunescope/widgets/selectionmenu.py
@@ -1,4 +1,5 @@
 import os.path
+import pdb
 
 from kivy.clock import Clock
 from kivy.uix.boxlayout import BoxLayout
@@ -23,25 +24,25 @@ class SelectionMenu(BoxLayout):
 
     def render_cells(self, *args):
         num_selections = len(self.selection_list.selections)
-        rows_to_add = num_selections - len(self.cells)
-        if rows_to_add > 0:
-            for i in range(rows_to_add):
-                cells_row = [Cell() for i in range(5)]
-                icon = TrashIcon(name='ios-trash', selection_index=num_selections + i)
+        num_rows = len(self.cells)
+        if num_selections > num_rows:
+            for i in range(num_rows, num_selections):
+                cells_row = [Cell() for j in range(5)]
+                icon = TrashIcon(name='ios-trash', selection_index=i)
                 cells_row[-1].add_widget(icon)
                 icon.bind(on_release=self.delete)
                 self.cells.append(cells_row)
                 for cell in cells_row:
                     self.ids.grid.add_widget(cell)
 
-        elif rows_to_add < 0:
-            for i in range(-rows_to_add):
-                cells_row = self.cells[i]
+        elif num_selections < num_rows:
+            for i in range(num_rows - num_selections):
+                cells_row = self.cells.pop()
                 self.ids.grid.clear_widgets(children=cells_row)
 
         for i, s in enumerate(self.selection_list.selections):
             self.cells[i][0].text = str(s.number)
-            self.cells[i][1].text = str(s.name)
+            self.cells[i][1].text = s.name or 'untitled'
             self.cells[i][2].text = str(s.start)
             self.cells[i][3].text = str(s.end)
 
@@ -61,6 +62,7 @@ class SelectionMenu(BoxLayout):
         self.render_cells()
 
     def delete(self, icon):
+        # pdb.set_trace()
         selection = self.selection_list.selections[icon.selection_index]
         self.selection_list.delete(selection)
         self.render_cells()
